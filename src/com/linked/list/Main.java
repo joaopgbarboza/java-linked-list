3ce24984e88767e85849b7d5c558bd6a1e0bf9d1
João Pedro Barboza <75705821+joaopgbarboza@users.noreply.github.com>
2025-04-25 04:18:45 +0100

    First commit

---
+++ src/com/linked/list/Main.java
@@ -0,0 +1,11 @@
+package com.linked.list;
+
+public class Main {
+
+    public static <Linkedlist> void main(String[] args){
+        LinkedList myLinkedList = new LinkedList(7);
+
+        myLinkedList.printAll();
+    }
+
+}
