8e9515c33d632cb4b053b90b0b55b29777121059
João Pedro Barboza <75705821+joaopgbarboza@users.noreply.github.com>
2025-05-03 21:42:50 +0100

    adding get method

--- src/com/linked/list/LinkedList.java
+++ src/com/linked/list/LinkedList.java
@@ -60,19 +60,78 @@ public class LinkedList {
         }
     }
 
+    //Append after this part
+
     public void append(int value){
         Node newNode = new Node(value);
         if(length == 0){
             head = newNode;
-            tail = newNode;
         } else {
             tail.next = newNode;
-            tail = newNode;
         }
+        tail = newNode;
         length++;
-
-
 
     }
 
-}
+    //Remove Last after this part
+
+    public Node removeLast() {
+
+        if (length == 0) return null;
+        Node pre = head;
+        Node temp = head;
+
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+
+        }
+
+        public void prepend(int value){
+            Node newNode = new Node(value);
+            if (length == 0) {
+             head = newNode;
+             tail = newNode;
+            } else {
+                newNode.next = head;
+                head = newNode;
+            }
+            length++;
+
+        }
+
+        public Node removeFirst(){
+            if (length == 0) return null;
+            Node temp = head;
+            head = head.next;
+            temp.next = null;
+            length--;
+            if (length == 0) {
+                tail = null;
+            }
+            return temp;
+        }
+
+        public Node get(int index) {
+            if(index < 0 || index >= length) return null;
+            Node temp = head;
+            for(int i = 0; i < index; i++){
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+    }
+
+
+
--- src/com/linked/list/Main.java
+++ src/com/linked/list/Main.java
@@ -2,14 +2,17 @@ package com.linked.list;
 
 public class Main {
 
-    public static <Linkedlist> void main(String[] args){
-        LinkedList myLinkedList = new LinkedList(1);
+    public static void main(String[] args){
+        LinkedList myLinkedList = new LinkedList(0);
 
-        myLinkedList.append(5);
-        myLinkedList.append(7);
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
 
 
-        myLinkedList.printAll();
+        System.out.println(myLinkedList.get(2).value + "\n");
+
+        myLinkedList.printList();
 
 
     }
