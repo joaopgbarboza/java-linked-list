cc830d847e3335c3a9fc481847699b33a3068740
João Pedro Barboza <75705821+joaopgbarboza@users.noreply.github.com>
2025-05-03 22:24:53 +0100

    adding insert and remove method

--- src/com/linked/list/LinkedList.java
+++ src/com/linked/list/LinkedList.java
@@ -131,7 +131,48 @@ public class LinkedList {
             return temp;
         }
 
-    }
+        public boolean set(int index, int value) {
+            Node temp = get(index);
+            if (temp != null) {
+                temp.value = value;
+                return true;
+            }
+            return false;
+        }
+
+        public boolean insert(int index, int value){
+            if(index < 0 || index > length) return false;
+            if (index ==0){
+                prepend(value);
+                return true;
+            } if (index == length) {
+                append(value);
+                return true;
+            }
+            Node newNode = new Node(value);
+            Node temp = get(index -1);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            length++;
+            return true;
+        }
+
+        public Node remove(int index) {
+            if (index < 0 || index > length) return null;
+            if (index == 0) return removeFirst();
+            if (index == length -1) return removeLast();
+            Node pre = get(index - 1);
+            Node temp = pre.next;
+            pre.next= temp.next;
+            temp.next = null;
+            length--;
+            return temp;
+
+
+        }
+
+
+}
 
 
 
--- src/com/linked/list/Main.java
+++ src/com/linked/list/Main.java
@@ -5,12 +5,13 @@ public class Main {
     public static void main(String[] args){
         LinkedList myLinkedList = new LinkedList(0);
 
-        myLinkedList.append(1);
+
         myLinkedList.append(2);
-        myLinkedList.append(3);
 
 
-        System.out.println(myLinkedList.get(2).value + "\n");
+        myLinkedList.insert(1, 1);
+
+        System.out.println(myLinkedList.remove(0).value + "\n");
 
         myLinkedList.printList();
 
